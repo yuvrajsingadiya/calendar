130426d5f31a786765836fb707e7872b6bb36b18
Yuvraj Singadiya <143711226+yuvrajsingadiya@users.noreply.github.com>
2023-09-27 01:42:10 +0530

    calandor
    no

---
+++ index.js
@@ -0,0 +1,41 @@
+const monthE1 = document.querySelector(".date h1")
+const fullDateE1 = document.querySelector(".date p")
+const daysE1 = document.querySelector(".days")
+
+
+
+const monthInx = new Date().getMonth();
+const lastDay = new Date(new Date() .getFullYear(), monthInx + 1, 0).getDate();
+const firstday = new Date(new Date() .getFullYear(), monthInx, 1).getDay() - 1; 
+console.log(firstday);
+const months = [
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December",
+];
+
+monthE1.innerText = months[monthInx]
+fullDateE1.innerText = new Date().toDateString();
+
+let days = "";
+
+for(let i = firstday; i > 0; i--){
+    days += `<div class="empty"></div>`;
+}
+for (let i=1; i <=lastDay; i++){
+    if(i === new Date().getDate()) {
+        days +=  `<div class="today">${i}</div>`;
+    }
+days +=  `<div>${i}</div>`;
+}
+
+daysE1.innerHTML = days;
